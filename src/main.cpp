ff67fe5e14981e3244dfd6bf43ded85174093e16
xelavoprak <karpovav@list.ru>
2018-02-08 17:59:41 +0300

    add files

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include "simple.hpp"
+
+int main(int argc, char *argv[])
+{
+    Simple hi;
+    hi.print();
+    return 0;
+}
---
+++ src/simple.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "simple.hpp"
+
+void Simple::print()
+{
+    std::cout << "Hello Static Library!" << std::endl;
+}
